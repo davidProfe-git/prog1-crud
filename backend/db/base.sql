19954d122086e1c14aaeac970e001232786d8488
david rugeles <david.rugeles@pi.edu.co>
2026-03-25 10:38:23 -0500

    consumo base de datos

---
+++ backend/db/base.sql
@@ -0,0 +1,1 @@
+SELECT * FROM recetas where id = 4
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express = require("express")
+const controlador = require("./controllers/recetaController")
 const aplicacion = express()
 
 aplicacion.get('/',(request, response)=>{
-    response.send(`<h1>Hola de ${clase} 1</h1>`)
+    // response.send(`<h1>Hola de ${clase} 1</h1>`)
 })
 
-aplicacion.get('/saludo',(request, response)=>{
-    response.json({mensaje: "hola muchachx"})
+aplicacion.get('/recetas',(request,response)=>{
+    controlador.obtenerRecetas(response)
 })
+
 
 aplicacion.listen(3000,()=>{
     console.log("el servidor levanto")
