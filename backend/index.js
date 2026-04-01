2321dd9709c50c9743e0e269be67ba90f6ded306
david rugeles <david.rugeles@pi.edu.co>
2026-04-01 13:00:52 -0500

    cambios

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express")
+const cors = require("cors")
 const controlador = require("./controllers/recetaController")
 const aplicacion = express()
 
+aplicacion.use(cors())
 aplicacion.get('/',(request, response)=>{
     // response.send(`<h1>Hola de ${clase} 1</h1>`)
 })
